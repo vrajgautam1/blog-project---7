e27d0d4ae0e501fff1fa54db2bb901d5ca4f58a2
vrajgautam1 <vrajgautam123@gmail.com>
2025-03-11 10:19:26 +0530

    added vercel configuration

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ const path = require('path');
 const fs = require('fs');
 const cookieParser = require('cookie-parser');
 
-
 app.set('view engine', 'ejs');
 app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
-app.use(cookieParser())
+app.use(cookieParser());
 
 const mainRouter = require('./router/index');
 app.use(mainRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    db(); 
-    console.log(`Server is running on port ${PORT}`);
-    console.log("http://localhost:"+PORT);
-});
+// Check if running locally
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        db();
+        console.log(`Server is running on port ${PORT}`);
+        console.log("http://localhost:" + PORT);
+    });
+}
+
+// Export app for Vercel
+module.exports = app;
